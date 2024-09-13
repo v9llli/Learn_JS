042d80621b3484a63532b0e2b1ed2cb3142ca982
v9llli <145156122+v9llli@users.noreply.github.com>
2024-09-13 15:09:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 101,
+    }),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+    // fetch('https://jsonplaceholder.typicode.com/posts/101')
